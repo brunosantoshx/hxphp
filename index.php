47d78279de8afd35fc212262f48644b85d0b5be3
Bruno Santos <bruno@plug.ag>
2017-12-16 19:48:50 -0200

    Updated version

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ define('ROOT_PATH', dirname(__FILE__) . DS);
 define('APP_PATH', 'app' . DS);
 define('TEMPLATES_PATH', ROOT_PATH . 'templates' . DS);
 
-define('HXPHP_VERSION', '3.0.0-rc.4');
+define('HXPHP_VERSION', '3.0.0-rc.5');
 
 /**
  * Verifica se o autoload do Composer está configurado
