cd4f0810c03379a656978aef0ec8bd033dad98f7
Bruno Santos <bruno@plug.ag>
2018-02-25 20:16:33 -0300

    Removed hxphp directory for default

--- app/config.php
+++ app/config.php
@@ -4,6 +4,7 @@ $configs = new HXPHP\System\Configs\Config;
 ActiveRecord\Connection::$datetime_format = 'Y-m-d H:i:s';
 
 $configs->env->add('development');
+$configs->env->development->baseURI = '/';
 
 
 /*
