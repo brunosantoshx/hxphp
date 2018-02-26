91f4b99769a698ca1266cc39f0ebb4286debc66e
Bruno Santos <bruno@plug.ag>
2018-02-25 21:32:51 -0300

    Preparing to test the application

--- tests/HXPHP/BaseTestCase.php
+++ tests/HXPHP/BaseTestCase.php
@@ -2,9 +2,39 @@
 
 namespace Tests;
 
+use HXPHP\System\App;
+use HXPHP\System\Configs\Config;
 use PHPUnit\Framework\TestCase;
 
 abstract class BaseTestCase extends TestCase
 {
+	protected $baseURI = 'https://hxphp.dev';
 
+	private $configs;
+
+	protected $app;
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->createConfigs()
+				->createApplication();
+	}
+
+	private function createConfigs()
+	{
+		$this->configs = new Config;
+		$this->configs->env->add('development');
+		$this->configs->env->development->baseURI = $this->baseURI;
+
+		return $this;
+	}
+
+	public function createApplication()
+	{
+		$this->app = new App($this->configs);
+
+		return $this;
+	}
 }
