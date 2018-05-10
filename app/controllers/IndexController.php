6835a8208f3ea58d5217662b3f2a133381ac2edd
Bruno Santos <bruno@plug.ag>
2018-05-09 21:28:57 -0300

    Added test response

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,6 +8,14 @@ class IndexController extends Controller
 {
     public function testAction()
     {
-    	
+    	$response = new Response();
+
+		$response->setContent(json_encode(array(
+		    'data' => new \stdClass(),
+		)));
+		
+		$response->headers->set('Content-Type', 'application/json');
+
+		return $response;
     }
 }
